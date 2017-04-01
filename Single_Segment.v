4c51669fad75c00d6f59157bb50dec932b879223
Luke Barbier <lukebarbier12@gmail.com>
2017-04-01 17:48:04 -0600

    Update Single_Segment.v

--- Single_Segment.v
+++ Single_Segment.v
@@ -4,8 +4,6 @@
 
 module Single_Segment(Case, Out);
 
-//Source: referencedesigner.com
-
 	input [3:0]Case;
 	output reg [6:0] Out;
 	
@@ -23,11 +21,11 @@ module Single_Segment(Case, Out);
 		4'b1000: Out=7'b0000000;
 		4'b1001: Out=7'b0010000;
 		4'b1010: Out=7'b0001000;
-		4'b1011:	Out=7'b0000011;
-		4'b1100:	Out=7'b1000110;
-		4'b1101:	Out=7'b0100001;
+		4'b1011: Out=7'b0000011;
+		4'b1100: Out=7'b1000110;
+		4'b1101: Out=7'b0100001;
 		4'b1110: Out=7'b0000110;
-		4'b1111:	Out=7'b0001110;
+		4'b1111: Out=7'b0001110;
 		default: Out=7'b1111111;
 	endcase
 	
