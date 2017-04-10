5852e4d17b51d02cc38a6c85b9be623e9eea8e65
Luke Barbier <lukebarbier12@gmail.com>
2017-04-10 07:21:16 -0600

    Delete reaction_timer.v

--- reaction_timer.v
+++
@@ -1,8 +0,0 @@
-module Project_2_top (button, Clk, led, seg_sec, seg_tsec, seg_hsec);
-	input [1:0] button;
-	input Clk;
-	output led;
-	output [7:0] seg_sec;
-	output [6:0] seg_tsec, seg_hsec;
-	
-endmodule 
